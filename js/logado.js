bc909baf65e7fa08c0fc356d90dde826532b2a88
Bouchra Akl <bushraakl1234@gmail.com>
2022-10-28 12:35:56 -0300

    vrs inicial do js feita

--- js/logado.js
+++ js/logado.js
@@ -1,2 +1,20 @@
-let theQuantity = document.querySelector(".elemento");
+let theBox = document.querySelectorAll(".elemento");
+let theQuantity = document.querySelectorAll(".quantidade");
+
+
+
+for(let i =0 ; i<theQuantity.length ; i++){
+    if(parseInt(theQuantity[i].innerHTML) === 0){
+        theBox[i].style.cssText = "background-color:red";
+    }
+    else if (parseInt(theQuantity[i].innerHTML)> 0 && parseInt(theQuantity[i].innerHTML) < 10 ){
+        theBox[i].style.cssText = "background-color:#F7A81B";
+    }
+    else{
+        theBox[i].style.cssText = "background-color:#06AA2A";
+    }
+}
+
+
+
 
