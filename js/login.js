ab3d0b28f6f93d4c6576a0aaaa4af1b626c9289d
jean - home <jeanfelipe0610@gmail.com>
2022-11-01 22:11:39 -0300

    correcao

--- js/login.js
+++ js/login.js
@@ -7,6 +7,6 @@ function login(){
     var user = document.getElementById('user');
     var pwd = document.getElementById('pwd');
     if(user.value == "admin" && pwd.value == "admin"){
-        window.location.href = "https://grupo09-projeto-integrador.github.io/GPC/GPC/pages/home/home.html";
+        window.location.href = "https://grupo09-projeto-integrador.github.io/GPC/pages/home/home.html";
     }
 }
