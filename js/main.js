c29139d757b36468faa8564aa016eb3173c52e91
vainicius <comercial.viniciusgabriel@gmail.com>
2022-11-21 01:34:45 -0300

    resolução de conflitos

--- js/main.js
+++ js/main.js
@@ -9,12 +9,18 @@ Autores:
 const head = document.querySelector("head");
 const body = document.querySelector("body");
 
-head.insertAdjacentHTML("beforeend", `<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>
+head.insertAdjacentHTML(
+  "beforeend",
+  `<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>
 <link rel='stylesheet' href='../../css/main.css'>
+<link rel='stylesheet' href='../../css/notifications.css'>
 <link rel='shortcut icon' href='../../assets/favicon.svg' type='image/x-icon'>
-`);
+`
+);
 
-body.insertAdjacentHTML("afterbegin", `<sidebar class="navMenu">
+body.insertAdjacentHTML(
+  "afterbegin",
+  `<sidebar class="navMenu">
 <figure><img class="logo-sidebar" src="../../assets/imgs/logo-rt-int-2.png" alt="Sistema GPC Rotary"></figure>
 <nav>
     <ul>
@@ -23,14 +29,36 @@ body.insertAdjacentHTML("afterbegin", `<sidebar class="navMenu">
         <li><a href="../ativos/ativos.html"><i class="bi bi-archive-fill"></i> Ativos</a></li>
         <li><a href="../relatorios/relatorios.html"><i class="bi bi-archive-fill"></i> Relatórios</a></li>
         <li><a href="../ajustes/ajustes.html"><i class="bi bi-gear-fill"></i> Ajustes</a></li>
-        <li><a href="#"><i class="bi bi-bell-fill"></i> Notificações</a></li>
+        <li><a href="#"><i class="bi bi-bell-fill"></i><button class="btnOpenModal" onclick="openModal()">Notificações</button></a></li>
     </ul>
 </nav>
-
+<div class="modal-container">
+        <div class="modal">
+        
+            <h2>Notificações</h2>
+            <span>
+            Texto teste
+            </span>
+            <div class="btns">
+                <button class="btnClose" onclick="closeModal()">Close</button>
+            </div>
+        </div> 
+     </div>
 <nav class="logout">
     <ul>
         <li><a href="../../index.html"><i class="bi bi-box-arrow-right"></i> Deslogar</a></li>
     </ul>
 </nav>
-</sidebar>`);
+</sidebar>`
+);
+
+const modal = document.querySelector(".modal-container");
+
+function openModal() {
+  modal.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+}
 
